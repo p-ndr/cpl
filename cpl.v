cee4c2464ff4bc2fa20fc6f3d614b68c3753db86
Parnian Naderi <negarintarlan@gmail.com>
2023-10-20 19:02:19 +0330

    added notations, needs fix.

--- cpl.v
+++ cpl.v
@@ -1,5 +1,3 @@
-(* Import Nat. *)
-
 Require Import List.
 Import List.ListNotations.
 
@@ -14,20 +12,25 @@ Inductive formula : Type :=
 | top : formula
 | bot : formula.
 
-Notation "⊥" := bot (at level 200).
-Notation "⊤" := top (at level 200).
+Notation "⊥" := bot (at level 95).
+Notation "⊤" := top (at level 95).
+Infix "→" := arrow (at level 90).
+Infix "∨" := disjunction (at level 85).
+Infix "∧" := conjunction (at level 85).
+Notation "¬" := negation (at level 80).
+
 
 Definition context : Type := list formula. 
 
 Fixpoint eqf (A B : formula) : bool :=
     match A, B with
     | (atom a), (atom b) => PeanoNat.Nat.eqb a b
-    | (conjunction a a'), (conjunction b b') => andb (eqf a b) (eqf a' b')
-    | (disjunction a a'), (disjunction b b') => andb (eqf a b) (eqf a' b')
-    | (arrow a a'), (arrow b b') => andb (eqf a b) (eqf a' b')
-    | (negation a), (negation b) => (eqf a b)
-    | top, top => true 
-    | bot, bot => true 
+    | (a ∧ a'), (b ∧ b') => andb (eqf a b) (eqf a' b')
+    | (a ∨ a'), (b ∨ b') => andb (eqf a b) (eqf a' b')
+    | (a → a'), (b → b') => andb (eqf a b) (eqf a' b')
+    | (¬a), (¬b) => (eqf a b)
+    | ⊤, ⊤ => true 
+    | ⊥, ⊥ => true 
     | _, _ => false 
     end.
 
@@ -37,24 +40,24 @@ Fixpoint checkprop (A : formula) (Γ : context) : bool :=
     | x :: l => if (eqf A x) then true else (checkprop A l)
     end.
 
+Reserved Notation "⊢" (at level 100).
+
 Inductive NP : context -> formula -> Prop :=
-| axiom : forall (Γ : context) (A : formula), (checkprop A Γ = true) -> (NP Γ A)
-| ande_left : forall (Γ : context) (A B : formula), (NP Γ (conjunction A B)) -> (NP Γ A)
-| ande_right : forall (Γ : context) (A B : formula), (NP Γ (conjunction A B)) -> (NP Γ B)
-| andi : forall (Γ : context) (A B : formula), (NP Γ A) -> (NP Γ B) -> (NP Γ (conjunction A B))
-| ore : forall (Γ : context) (A B C : formula),
-        (NP (A :: Γ) C) -> (NP (B :: Γ) C) -> (NP ((disjunction A B) :: Γ) C)
+| axiom : forall (Γ : context) (A : formula), (checkprop A Γ = true) -> (Γ ⊢ A)
+| ande_left : forall (Γ : context) (A B : formula), (Γ ⊢ (A ∧ B)) -> (Γ ⊢ A)
+| ande_right : forall (Γ : context) (A B : formula), (Γ ⊢ (A ∧ B)) -> (Γ ⊢ B)
+| andi : forall (Γ : context) (A B : formula), (Γ ⊢ A) -> (Γ ⊢ B) -> (Γ ⊢ (A ∧ B))
+| ore : forall (Γ : context) (A B C : formula), ((A :: Γ) ⊢ C) -> ((B :: Γ) ⊢ C) -> (((A ∨ B) :: Γ) ⊢ C)
 
-| ori_left : forall (Γ : context) (A B : formula), (NP Γ A) -> (NP Γ (disjunction A B))
-| ori_right : forall (Γ : context) (A B : formula), (NP Γ B) -> (NP Γ (disjunction A B))
-| MP : forall (Γ : context) (A B : formula), (NP Γ A) -> (NP Γ (arrow A B)) -> (NP Γ B)
-| arrowi : forall (Γ : context) (A B : formula), (NP (A :: Γ ) B) -> (NP Γ (arrow A B))
-| eneg : forall (Γ : context) (A : formula), (NP Γ A) -> (NP Γ (negation A)) -> (NP Γ (⊥))
-| ineg : forall (Γ : context) (A : formula), (NP (A :: Γ) (⊥)) -> (NP Γ (negation A))
-| RAA : forall (Γ : context) (A : formula), (NP ((negation A) :: Γ) (⊥)) -> (NP Γ A)
-| ibot : forall (Γ : context) (A : formula), (NP Γ (⊥)) -> (NP Γ A).
-
-Infix "⊢" := NP (at level 100).
+| ori_left : forall (Γ : context) (A B : formula), (Γ ⊢ A) -> (Γ ⊢ (A ∨ B))
+| ori_right : forall (Γ : context) (A B : formula), (Γ ⊢ B) -> (NP Γ (A ∨ B))
+| MP : forall (Γ : context) (A B : formula), (Γ ⊢ A) -> (Γ ⊢ (A → B)) -> (Γ ⊢ B)
+| arrowi : forall (Γ : context) (A B : formula), ((A :: Γ ) ⊢ B) -> (Γ ⊢ (A → B))
+| eneg : forall (Γ : context) (A : formula), (Γ ⊢ A) -> (Γ ⊢ (¬A)) -> (Γ ⊢ (⊥))
+| ineg : forall (Γ : context) (A : formula), ((A :: Γ) ⊢ (⊥)) -> (Γ ⊢ (¬A))
+| RAA : forall (Γ : context) (A : formula), (((¬A) :: Γ) ⊢ (⊥)) -> (Γ ⊢ A)
+| ibot : forall (Γ : context) (A : formula), (Γ ⊢ (⊥)) -> (Γ ⊢ A)
+where "x ⊢ y" := (NP x y).
 
 (* Lemma membership_preservation
 
